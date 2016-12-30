972cda746ff70362988676b172fe4b607c691796
z408550513 <z408550513@163.com>
2016-12-30 16:57:52 +0800

    Add files via upload

---
+++ ZeroMethodSwizzling/ZeroMethodSwizzling/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ZeroMethodSwizzling
+//
+//  Created by Zero on 2016/12/30.
+//  Copyright © 2016年 Zero. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        self.test1()
+        ZeroSwap(cls: ViewController.self , originalSelector: #selector(ViewController.test1), swizzledSelector: #selector(ViewController.test2))
+        self.test1()
+
+    
+       
+        
+        
+    }
+    
+    dynamic func test1() {
+        print("i m test1")
+        
+    }
+    
+    dynamic func test2() {
+        
+        print("u print test2 sure")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
